980e764875d203036878595106b57556c5dba89e
Asep Saepuloh <warkirasep.puloh@gmail.com>
2022-05-24 02:22:10 +0700

    Add Different Layout

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,38 @@
 
 <template>
-  <router-view></router-view>
+	<div v-if="!$route.meta.plainLayout">
+		<Topbar/>
+		<!-- Page content -->
+		<div class="page-content">
+			<Sidebar/>
+			<!-- Main content -->
+			<div class="content-wrapper">
+
+				<!-- Inner content -->
+				<div class="content-inner">
+					<router-view></router-view>
+					<Footer/>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- Main navbar -->
+	<template v-if="$route.meta.plainLayout">
+		<Login/>
+	</template>
 </template>
 <script>
- export default {
-   name: 'App'
- }
+	import Login from '../src/components/Login.vue'
+	import Topbar from '../src/components/Topbar.vue'
+	import Sidebar from '../src/components/Sidebar.vue'
+	import Footer from '../src/components/Footer.vue'
+	export default {
+		name: 'App',
+		components: {
+			Login,
+			Topbar,
+			Sidebar,
+			Footer
+		}
+	}
 </script>
--- src/router/index.js
+++ src/router/index.js
@@ -3,18 +3,32 @@ import { createRouter, createWebHashHistory, createWebHistory } from "vue-router
 const routes = [
     {
         path: '/',
+        name: 'main',
+        component: () => import ("../views/Home.vue"),
+        meta: {
+            plainLayout: false
+        }
+    },
+    {
+        path: '/home',
         name: 'home',
-        component: () => import ("../views/Home.vue")
+        component: () => import ("../views/Home.vue"),
+        meta: {
+            plainLayout: false
+        }
     },
     {
         path: '/login',
         name: 'login',
-        component: () => import ("../views/Login.vue")
+        component: () => import ("../components/Login.vue"),
+        meta: {
+            plainLayout: true 
+        }
     }
 ];
 
 const router = createRouter({
-    history: createWebHistory,
+    history: createWebHistory(),
     routes
 });
 
